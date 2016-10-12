6548266306a71e4bb6fe98c2c53ad12e15aa0ce3
shivasdst <shivably04sdst@gmail.com>
2016-10-12 15:24:12 +0530

    changed messages

--- application/config/messages.php
+++ application/config/messages.php
@@ -36,4 +36,7 @@ define('VLDTY_EMPTY_DATA', 'Fields marked with * are mandatory');
 define('VLDTY_EM_UREG', 'This email id seems to be already registered with us. Try logging in or use another id.');
 define('VLDTY_PW_NEQ', 'Passwords not in confirmation.');
 
+define('GIT_ADD_MSG', 'New Json for Albums / Photos added');
+define('GIT_MOD_MSG', 'Json for Albums / Photos modifed');
+define('GIT_DEL_MSG', 'Json for Albums / Photos deleted');
 ?>
