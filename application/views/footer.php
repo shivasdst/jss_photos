05eb914f388632f7c85606d464cea09918e2347a
shivasdst <shivably04sdst@gmail.com>
2017-04-27 17:05:26 +0530

    included images loaded plugin

--- application/views/footer.php
+++ application/views/footer.php
@@ -13,8 +13,10 @@
 
     <!-- End Main page layout
     –––––––––––––––––––––––––––––––––––––––––––––––––– -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/masonry/3.1.5/masonry.pkgd.min.js" async></script>
     
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/masonry/3.1.5/masonry.pkgd.min.js" async></script>
+	<script type="text/javascript" src="<?=PUBLIC_URL?>js/imagesloaded.pkgd.min.js"></script>
+     
     <script type="text/javascript" src="<?=PUBLIC_URL?>js/common.js" async></script>
    
 </body>
